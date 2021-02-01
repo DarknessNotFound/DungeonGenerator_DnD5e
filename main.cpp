e31ac0c2db6b46c9a7d2129c936058be8af55bb3
Albright <UM-AD\gra9hb@rc06xcs213.managed.mst.edu>
2021-02-01 17:19:56 -0600

    Header Only for main.cpp

---
+++ main.cpp
@@ -0,0 +1,5 @@
+// Programmer: DarknessNotFound
+// File:       main.cpp
+// Date:       2/1/2021
+// Purpose:    This is going to house the main loop for the overall program.
+//	       This will also include the basic text output for the end user.
